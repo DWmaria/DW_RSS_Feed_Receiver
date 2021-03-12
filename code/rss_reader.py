a1856f1b838008ec273ec12bb31aab84535361ac
DWmaria <63299301+DWmaria@users.noreply.github.com>
2021-03-12 19:56:32 +0100

    added languages and a first run

--- code/rss_reader.py
+++ code/rss_reader.py
@@ -4,29 +4,39 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 def dw_english_topStories_rss():
     article_list = []
-    try:
-        r = requests.get('https://rss.dw.com/rdf/rss-en-all')
-        soup = BeautifulSoup(r.content, features='xml')
-        articles = soup.findAll('item')
-        for a in articles:
-            title = a.find('title').text
-            link = a.find('link').text
-            published = a.find('dc:date').text
-            language = a.find('dc:language').text
-            oid = a.find('dwsyn:contentID').text
-            article = {
-                'oid': oid,
-                'title': title,
-                'link': link,
-                'published': published,
-                'language': language
-            }
-            article_list.append(article)
-        print(len(article_list))
-        return save_to_txt(article_list)
+    #nicht gefunden: bengali, bulgarisch, kroatisch griechisch hindi kiswahili mazedonisch urdu
+    language_list = ['en-all', 'de-all', 'alb-all', 'amh-news', 'ar-all', 'bos-all', 'chi-all',
+                     'dar-all', 'fre-all',  'br-africa','sp-all', 'hau-nr', 'ind-all', 'pas-all',
+                     'per-all', 'pol-all', 'br-all', 'rom-all', 'ru-all', 'serbian_all',  'tur-all',
+                     'ukr-all']
 
+    try:
+        for lang in language_list:
+                frag = ''
+                if lang != 'serbian_all': frag = 'rss-'
+                url = 'https://rss.dw.com/rdf/'+frag+lang
+                r = requests.get(url)
+                soup = BeautifulSoup(r.content, features='xml')
+                articles = soup.findAll('item')
+                for a in articles:
+                    title = a.find('title').text
+                    link = a.find('link').text
+                    published = a.find('dc:date').text
+                    language = a.find('dc:language').text
+                    oid = a.find('dwsyn:contentID').text
+                    article = {
+                        'oid': oid,
+                        'title': title,
+                        'link': link,
+                        'published': published,
+                        'language': language
+                    }
+                    article_list.append(article)
+                print(lang + ': ' + str(len(articles)))
+        save_to_txt(article_list)
     except Exception as e:
         print('The scraping job failed. See exception: ')
         print(e)
